c4d04b641f8e737ded938b24e8b5cacefe68a6c5
NightCrawler2 <fardinramezani97@gmail.com>
2023-10-16 11:50:03 +0330

    add custom user model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('pkid', 'id', 'email', 'username', 'is_staff', 'is_active')
+    list_display_links = ('id', 'email')
+    list_filter = ('email', 'username', 'is_staff', 'is_active')
+    fieldsets = (
+        (
+            "Login Credentials",
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Personal Information",
+            {
+                "fields": (
+                    "username",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Permissions and Groups",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+    search_fields = ("email", "username", "first_name", "last_name")
+
+
+admin.site.register(User, UserAdmin)
---
+++ apps/users/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = User
+        fields = ["email", "username", "first_name", "last_name"]
+        error_class = "error"
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ["email", "username", "first_name", "last_name"]
+        error_class = "error"
